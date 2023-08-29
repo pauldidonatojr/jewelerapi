470630fbcab7290ec7d2fae8a8f35cfcda02a8a4
pauldidonatojr <pjdidonatojr@gmail.com>
2023-08-29 14:21:14 -0400

    push me

--- data.js
+++ data.js
@@ -277,7 +277,7 @@ const data = [
       "id": "recm7wC8TBVdU9oEL",
       "name": "wooden desk",
       "price": 40099,
-      "image": "https://res.cloudinary.com/dwyqdiqds/image/upload/v1688843492/04-removebg-preview_kv6vdd.png",
+      "image": "https://res.cloudinary.com/dwyqdiqds/image/upload/v1693325646/cassello%20jewelers/Cassello%20Jeweler/F9CCB2E6-5AD2-48A5-B216-17FC149D0C3D_rhvbzy.jpg",
       "colors": [
         "#0000ff",
         "#00ff00"
