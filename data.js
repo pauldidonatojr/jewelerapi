6a70b42210c704f2bc1c03a824a2e96c00e0e6a1
pauldidonatojr <pjdidonatojr@gmail.com>
2023-08-29 13:26:41 -0400

    push me

--- data.js
+++ data.js
@@ -25,7 +25,7 @@ const data = [
       ],
       "company": "liddy",
       "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
-      "category": "living room"
+      "category": "Earrings"
     },
     {
       "id": "rec5NBwZ5zCD9nfF0",
