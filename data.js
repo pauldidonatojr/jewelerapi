295e55df6f70a155bea8fbb8b1eda282e94fda03
pauldidonatojr <pjdidonatojr@gmail.com>
2023-08-29 18:23:28 -0400

    push me

--- data.js
+++ data.js
@@ -24,7 +24,7 @@ const data = [
         "#ffb900"
       ],
       "company": "liddy",
-      "description": "Cloud bread VHS hell of banjo bicycle rights jianbing umami mumblecore etsy 8-bit pok pok +1 wolf. Vexillologist yr dreamcatcher waistcoat, authentic chillwave trust fund. Viral typewriter fingerstache pinterest pork belly narwhal. Schlitz venmo everyday carry kitsch pitchfork chillwave iPhone taiyaki trust fund hashtag kinfolk microdosing gochujang live-edge",
+      "description": "Drench yourself in the warmth of these radiant citrine crystal earrings, capturing the essence of sunshine and prosperity in each glint. Hand-selected for their mesmerizing yellow hues, these gemstones shimmer with a brilliance that elevates any outfit.",
       "category": "earrings"
     },
     {
