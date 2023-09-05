970c9c83aecb400c4a79beb6d29051e43b6d98f4
Dawar Mustaqeem <45918546+dmustaqeem@users.noreply.github.com>
2023-09-05 06:35:03 +0500

    updated value

--- data.js
+++ data.js
@@ -289,7 +289,7 @@ const data = [
     {
       "id": "RjewlMtrnZbLkQVG4",
       "name": "Sterling Silver 2CT Garnet & 0.20CT Diamond Ring",
-      "price": 85000,
+      "price": 850,
       "image": "https://res.cloudinary.com/dwyqdiqds/image/upload/v1693325645/cassello%20jewelers/Cassello%20Jeweler/DD936C8A-14E8-4AD4-B248-CE4D57F49A66_yrqwu0.jpg",
       "featured": true,
       "colors": [
